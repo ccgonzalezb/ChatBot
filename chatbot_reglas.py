2fa15198d8148022aa4f219b9eea062874f6a2bf
ccgonzalezb <ccgonzalezb1@gmail.com>
2025-05-19 12:42:57 -0500

    ChatBot con uso de diccionarios

--- chatbot_reglas.py
+++ chatbot_reglas.py
@@ -1,22 +1,27 @@
+#Mensajes de bienvenida y despedida
 print("Hola, soy un chatbot basado en reglas.")
 print("Escribe Salir para terminar la conversación.")
 
+#Diccionario con reglas
+respuestas = {
+    "hola":"¡Hola! ¿Cómo estás?",
+    "¿cómo estás?":"Estoy bien, gracias por preguntar.",
+    "como estas":"Estoy bien, gracias por preguntar.",
+    "¿qué puedes hacer?":"Puedo responder preguntas simples, como esta.",
+    "que puedes hacer":"Puedo responder preguntas simples, como esta.",
+    "adiós":"¡Hasta luego!",
+    "adios":"¡Hasta luego!",
+}
+#Bucle de conversación
 while True:
     user_input = input("Tú: ")
-    user_input = user_input.lower()# convertimos a minúsculas para facilitar la comparación
+    user_input = user_input.lower().strip()# convertimos a minúsculas para facilitar la comparación
 
-    if user_input == "hola":
-        print("Chatbot: ¡Hola! ¿Cómo estás?")
-    elif user_input == "¿Cómo estás?" or user_input == "como estas":
-        print("Chatbot: Estoy bien, gracias por preguntar.")
-    elif user_input == "¿qué puedes hacer?" or user_input == "que puedes hacer":  
-        print("Chatbot: Puedo responder preguntas simples, como esta.")
-    elif user_input == "adiós" or user_input == "adios":
-        print("Chatbot: ¡Hasta luego!")
-        break
-    elif user_input == "salir":
+    if user_input == "salir":
         print("Chatbot: Conversación finalizada.")
         break
+    elif user_input in respuestas:
+        print("Chatbot:", respuestas[user_input])
     else:
         print("Chatbot: Lo siento, no entiendo esa pregunta.")
 # Fin del código
